072b3032c6278daa3f77fc62e4efe2fb91fccc40
contolini <contolini@users.noreply.github.com>
2016-08-24 03:22:37 -0400

    Atomize cf-tables

--- src/cf-tables/src/cf-tables.js
+++ src/cf-tables/src/cf-tables.js
@@ -1,164 +1,155 @@
-/**
- * cf-tables
- * https://github.com/cfpb/cf-tables
- *
- * A public domain work of the Consumer Financial Protection Bureau
- */
 
-global.jQuery = require('jquery');
+'use strict';
 
-( function( $ ) {
-  'use strict';
+var closest = require( 'atomic-component/utilities/dom-closest' ).closest;
+var config = require( 'atomic-component/utilities/config' );
+var Organism = require( 'atomic-component/components/organisms/Organism' );
 
-  var SortableTable = function( table, options ) {
-    /*  At the moment, there are no default settings, but here's an object
-        for their future use! */
-    var defaults = {},
-    // settings is defaults combined with user options
-    settings = {},
-    // rows is an Array of Arrays, serving as a model of the table
-    rows = [],
-    $table = $( table ),
-    $sortButtons = $table.find( '.sortable' );
+var DIRECTIONS = config.DIRECTIONS;
+var UNDEFINED = config.UNDEFINED;
 
-    /**
-     * Initializes the SortableTable
-     * @param { object } options - Customizble options object
-     */
-    function _init( options ) {
-      settings = $.extend( {}, defaults, options );
-      _clickHandler();
-      // If the following classes exist, start by sorting those columns.
-      $table.find( '.sortable__start-up, .sortable__start-up' ).click();
-    }
+var TableOrganism = Organism.extend( {
 
-    /**
-     * Sorting function for Array.sort()
-     *
-     * @param { number } sign - A number where a negative number indicates a
-     * reverse sort.
-     * @param { sortType } sortType - A string used for sort types. By default,
-     * the values are sorted by their native type. If this value is set to
-     * 'number', then the cells' numeric values are used.
-     * @returns function - A function to be used by the Array.sort method, where
-     * the parameters 'a' and 'b' is each an Array (of Arrays) to be sorted
-     */
+  classes: {
+    baseElement: '.table__sortable',
+    sortDown:    'sort-down',
+    sortUp:      'sort-up'
+  },
 
-    function _arraySorter( sign, sortType ) {
-      return function( a, b ) {
-        // Set a and b to the first Array in each Array-of-Arrays
-        a = a[0];
-        b = b[0];
+  events: {
+    'click .sortable': 'sortTable'
+  },
 
-        // For number sort, convert a & b to numbers.
-        if ( sortType === 'number' ) {
-          a = Number( a.replace( /[^\d.-]/g, '' ) );
-          b = Number( b.replace( /[^\d.-]/g, '' ) );
-        }
+  ui: {
+    sortables:        '.sortable',
+    tableBody:        'tbody',
+    activeSortButton: null
+  },
 
-        // Sort the values
-        if ( a < b ) {
-          return sign * -1;
-        }
-        if ( a > b ) {
-          return sign;
-        }
-        return 0;
-      };
-    }
+  initialize: function() {
+    this.isTableSorted = false;
+    this.sortColumnIndex = UNDEFINED;
+    this.sortDirection = UNDEFINED;
+    this.tableData = [];
+  },
 
-    /**
-     * Updates internal model of table (rows[])
-     * @param { number } index - The index of the column used for sorting,
-     * which is used as the "key" for rows[] - it is set as the only value
-     * in the first array.
-     */
-    function _getRows( index ) {
-      var child = index + 1;
-      // Clear the model
-      rows.length = 0;
-      // Find the value in each row of the column we're sorting by,
-      // add it to the rows Array
-      $table.find( 'tbody tr' ).each( function() {
-        // indices count from 0, but nth-child counts from 1
-        var key = $( this ).find( 'td:nth-child(' + child + ')' ).text().trim();
-        rows.push( [ key, $( this ) ] );
-      } );
-    }
+ /**
+   * Updates the table in the DOM
+   * @param { number } columnIndex - The index of the column used for sorting
+   */
+  getTableData: function ( columnIndex ) {
+    columnIndex = columnIndex || this.sortColumnIndex;
+    var cell;
 
-    /**
-     * Updates the table in the DOM
-     * @param { number } index - The index of the column used for sorting
-     */
-    function _updateTable( index ) {
-      // Empty the tbody to prepare for sorted rows
-      $table.find( 'tbody' ).empty();
+    // Find the value in each row of the column we're sorting by,
+    // add it to the rows Array
+    var rows = this.ui.tableBody.querySelectorAll( 'tr' );
+    this.tableData = [];
 
-      // Insert sorted rows
-      for ( var i = 0; i < rows.length; i++ ) {
-        $table.find( 'tbody' ).append( rows[i][1] );
+    for ( var i = 0; i < rows.length; ++i ) {
+      cell = rows[i].cells[ columnIndex ];
+      if( cell ) {
+        cell = cell.textContent.trim();
       }
+      this.tableData.push( [ cell, rows[ i ] ] );
     }
 
-    /**
-     * Handler for click events on the column header
-     * No parameters - uses SortableTable properties, updates the DOM.
-     */
-    function _clickHandler() {
-      $sortButtons.on( 'click', function() {
-        var $button = $( this ),
-            $headercell = $button.closest( 'th, td' ),
-            $table = $headercell.closest( '.table__sortable' ),
-            sortType = $button.attr( 'data-sort_type' ),
-            sign = 1,
-            $firstChild = $table.find( 'tr:first-child' ),
-            index = $firstChild.children( 'th, td' ).index( $headercell );
+    this.tableData.sort( this.tableDataSorter( this.sortDirection ) )
 
-        _getRows( index );
+    return this.tableData;
+  },
 
-        // Determine sign
-        if ( $button.hasClass( 'sorted-up' ) || $button.hasClass( 'sortable__start-down' ) ) {
-          sign = -1;
-        }
+/**
+   * Sorting function for Array.sort()
+   *
+   * @param { number } direction - A number where a negative number indicates a
+   * reverse sort.
+   * @param { sortType } sortType - A string used for sort types. By default,
+   * the values are sorted by their native type. If this value is set to
+   * 'number', then the cells' numeric values are used.
+   * @returns function - A function to be used by the Array.sort method, where
+   * the parameters 'a' and 'b' is each an Array (of Arrays) to be sorted
+   */
+  tableDataSorter : function( direction, sortType ) {
+    return function( a, b ) {
+       var sign = 1;
+      // Set a and b to the first Array in each Array-of-Arrays
+      a = a[0];
+      b = b[0];
 
-        $sortButtons.removeClass( 'sortable__start-down sortable__start-up' );
-        $sortButtons.removeClass( 'sorted-down sorted-up' );
+      // For number sort, convert a & b to numbers.
+      if ( sortType === 'number' ) {
+        a = Number( a.replace( /[^\d.-]/g, '' ) );
+        b = Number( b.replace( /[^\d.-]/g, '' ) );
+      }
 
-        // Add correct class
-        if ( sign === 1 ) {
-          $button.addClass( 'sorted-up' );
-        } 
-        else {
-          $button.addClass( 'sorted-down' );
-        }
+      if ( direction === DIRECTIONS.DOWN ) {
+        sign = -1
+      }
 
-        // Perform the row sort
-        rows.sort( _arraySorter( sign, sortType ) );
+      // Sort the values
+      if ( a < b ) {
+        return sign * -1;
+      }
+      if ( a > b ) {
+        return sign;
+      }
+      return 0;
 
-        _updateTable( index );
-
-      } );
-    }
-
-    _init( options );
-
-  };
+    };
+  },
 
   /**
-   * Instatiates the SortableTable
-   * @param { object } options - An options object
-   * @returns { object } Attached objects for each matched element
+   * Updates the table in the DOM
    */
-  $.fn.sortableTable = function( options ) {
-    return this.each( function() {
-      ( options || ( options = {} ) ).$element = $( this );
-      var scol = new SortableTable( this, options );
-    } );
-  };
+  updateTable: function() {
+    var documentFragment;
+    var tableBody = this.ui.tableBody;
 
-  $( document ).ready( function() {
-    $( '.table__sortable' ).sortableTable();
-  } );
+    // Empty the table body to prepare for sorting the rows
+    // TODO: It might make sense to use innerHTML
+    // from a performance and garbage collection standpoint.
+    while ( tableBody.lastChild ) {
+      tableBody.removeChild( tableBody.lastChild );
+    }
 
+    // Insert sorted rows
+    documentFragment = document.createDocumentFragment();
+    for ( var i = 0; i < this.tableData.length; i++ ) {
+      documentFragment.appendChild( this.tableData[i][1] );
+    }
+    tableBody.appendChild( documentFragment );
 
-}( jQuery ) );
+    this.isTableSorted = true;
+
+    return this;
+  },
+
+  sortTable: function( event ) {
+    if( this.ui.activeSortButton ) {
+        this.removeClass( this.ui.activeSortButton,
+                          this.classes.sortedUp,
+                          this.classes.sortedDown );
+    }
+
+    if ( this.ui.activeSortButton === event.target ) {
+      this.sortDirection = ~this.sortDirection;
+    } else {
+      this.ui.activeSortButton = event.target;
+      this.sortColumnIndex = closest( event.target, 'td, th' ).cellIndex;
+      this.sortDirection = DIRECTIONS.UP;
+    }
+
+    var sortClass = ( this.sortDirection === DIRECTIONS.UP ) ? this.classes.sortUp : this.classes.sortDown;
+    this.addClass( this.ui.activeSortButton, sortClass );
+    this.getTableData();
+    this.updateTable();
+
+    return this;
+  }
+
+} );
+
+TableOrganism.constants.DIRECTIONS = DIRECTIONS;
+
+TableOrganism.init();
